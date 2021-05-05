065adc18d6c460a0a1e5523d366c39e6e04a58fe
swedishsailor <kzygowski7@gmail.com>
2021-05-05 20:28:18 +0200

    html li deleted, js li ready

--- js/script.js
+++ js/script.js
@@ -46,11 +46,7 @@ function titleClickHandler(event){
 
 }  
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
   
 
 const optArticleSelector = '.post',
@@ -60,19 +56,45 @@ optTitleListSelector = '.titles';
 function generateTitleLinks() {
 
  /* remove contents of titleList */
- //const titleList = optTitleListSelector.querySelector('')
+ const titleList = document.querySelector(optTitleListSelector);
+ titleList.innerHTML='';
 
   /* for each article */
 
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for (let article of articles){
+
     /* get the article id */
 
+    const articleId = article.getAttribute('id');
+
     /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* get the title from the title element */
 
     /* create HTML of the link */
 
+    const linktHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linktHTML);
+
     /* insert link into titleList */
+
+    html = html + linktHTML;
+    console.log(html);
+
+  }
+
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+console.log(links);
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
 
 }
   
