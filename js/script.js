4c547da3d2e89f543112807f9fa8aaa34e932115
swedishsailor <kzygowski7@gmail.com>
2021-05-05 16:42:14 +0200

    empty commit

--- js/script.js
+++ js/script.js
@@ -53,4 +53,27 @@ for(let link of links){
 }
   
 
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+ /* remove contents of titleList */
+ //const titleList = optTitleListSelector.querySelector('')
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
   
+generateTitleLinks();
